f5a124685f2c97fda2613c8046689590f5024a3b
huskyhehe <zhou.he1@northeastern.edu>
2022-06-21 01:49:00 -0700

    Update search service

--- src/main/java/com/mockcompany/webapp/service/SearchService.java
+++ src/main/java/com/mockcompany/webapp/service/SearchService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -56,6 +57,7 @@ public class SearchService {
                 itemList.add(item);
             }
         }
-        return itemList;
+
+        return Collections.emptyList();
     }
 }
