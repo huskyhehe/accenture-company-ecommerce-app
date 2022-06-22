1c3151f48306bc0e558bc09a9a5d71ed6426aba0
huskyhehe <zhou.he1@northeastern.edu>
2022-06-22 02:06:11 -0700

    Update image sources

--- client-app/src/server.js
+++ client-app/src/server.js
@@ -1,15 +1,15 @@
 import { createServer } from "miragejs"
 
-import image1 from "./images/1.png"
-import image2 from "./images/2.png"
-import image3 from "./images/3.png"
-import image4 from "./images/4.png"
-import image5 from "./images/5.png"
-import image6 from "./images/6.png"
-import image7 from "./images/7.png"
-import image8 from "./images/8.png"
-import image9 from "./images/9.png"
-import image10 from "./images/10.png"
+import image1 from "./images/1.jpg"
+import image2 from "./images/2.jpg"
+import image3 from "./images/3.jpg"
+import image4 from "./images/4.jpg"
+import image5 from "./images/5.jpg"
+import image6 from "./images/6.jpg"
+import image7 from "./images/7.jpg"
+import image8 from "./images/8.jpg"
+import image9 from "./images/9.jpg"
+import image10 from "./images/10.jpg"
 
 const productList = [
   { id: 1, description: "Stuff", cost: 201.0, image: image1 },
